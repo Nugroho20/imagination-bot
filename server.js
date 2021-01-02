c27e0e6c4216a1aab2ae1995dcc3b98f9c4bd459
Nugroho0002 <74390128+Nugroho0002@users.noreply.github.com>
2021-01-02 13:46:38 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const dbd = require("dbd.js")
+
+ 
+
+const bot = new dbd.Bot({
+
+token: "token bot kamu", 
+
+prefix: "prefix bot kamu" 
+
+})
+
+ 
+
+bot.onMessage()
+
+ 
+
+const fs = require('fs')
+
+const folders = fs.readdirSync("./commands/")
+
+for (const files of folders) {
+
+const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
+
+for (const commands of folder) {
+
+const command = require(`./commands/${files}/${commands}`) 
+
+bot.command({
+
+name: command.name,
+
+aliases: command.aliases,
+
+code: command.code
+
+})
+
+}
+
+}
