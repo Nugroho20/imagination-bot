156b0c385a04411e5b900aee816309c32b82ecd4
Nugroho <74390128+Nugroho20@users.noreply.github.com>
2021-01-03 23:21:53 +0700

    create suggestion command

---
+++ commands/suggest.js
@@ -0,0 +1,16 @@
+module.exports = {
+ mame: "suggestion",
+ aliases: ["suggest"],
+ code: `
+$title[New suggestion!]
+$description[
+$message
+]
+$color[RANDOM]
+$thumbnail[$authorAvatar]
+$addReactions[👍;👎]
+$deletecommand
+`
+}
+
+//You can change the reaction up to you
