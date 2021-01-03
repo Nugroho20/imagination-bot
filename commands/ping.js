a38d5800989a1dd09388e52d8266473563c491ed
Nugroho <74390128+Nugroho20@users.noreply.github.com>
2021-01-03 23:44:13 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,11 @@
 module.exports = ({
-
-name: "name",
-
-aliases: ['name2','name3','etc'],
-
-code: `
+  name: "name",
+  aliases: ['name2','name3','etc'],
+  code: `
 Get bot ping...
 $editIn[3s;Pong 🏓 = **__$ping__**ms
-
 `
 
 })
+
+//you can edit as you wish
