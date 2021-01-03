82d323187da1e79133f624153eb9240c19d5eba6
Nugroho <74390128+Nugroho20@users.noreply.github.com>
2021-01-03 23:51:53 +0700

    Update server.js
    Fixed handlers

--- server.js
+++ server.js
@@ -1,43 +1,22 @@
 const dbd = require("dbd.js")
 
- 
-
 const bot = new dbd.Bot({
-
-token: "token bot kamu", 
-
-prefix: "prefix bot kamu" 
-
+token: "token", 
+prefix: "prefix" 
 })
-
- 
 
 bot.onMessage()
 
- 
-
 const fs = require('fs')
-
 const folders = fs.readdirSync("./commands/")
-
 for (const files of folders) {
-
 const folder = fs.readdirSync(`./commands/${files}/`).filter(file => file.endsWith(".js"))
-
 for (const commands of folder) {
-
 const command = require(`./commands/${files}/${commands}`) 
-
 bot.command({
-
 name: command.name,
-
 aliases: command.aliases,
-
 code: command.code
-
 })
-
 }
-
 }
